789259c13e6071e8d1112ce3edf9a223229fb359
timberrr <timberrr@users.noreply.github.com>
2022-08-31 11:36:44 +0100

    removed unused FontAwesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@fortawesome/fontawesome-svg-core/styles.css'],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
