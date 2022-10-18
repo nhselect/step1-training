b15f83a61f0c68bd24da0ed3953773a52e0f3eec
timberrr <timberrr@users.noreply.github.com>
2022-10-18 17:33:26 +0100

    feedback pages and rolled back dart-sass to stop deprec errors

--- pages/feedback/_role.vue
+++ pages/feedback/_role.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <div class="nhsuk-back-link">
+      <a class="nhsuk-back-link__link" :href="'/roles/'+role.slug">
+        <svg class="nhsuk-icon nhsuk-icon__chevron-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" height="24" width="24">
+          <path d="M8.5 12c0-.3.1-.5.3-.7l5-5c.4-.4 1-.4 1.4 0s.4 1 0 1.4L10.9 12l4.3 4.3c.4.4.4 1 0 1.4s-1 .4-1.4 0l-5-5c-.2-.2-.3-.4-.3-.7z"></path>
+        </svg>
+        Back to training materials</a>
+    </div>
     <h1>
       Feedback required
       <span class="nhsuk-caption-xl">{{ role.title }}</span>
---
+++ pages/feedback/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-for="role in roles" :key="role.slug">
+      <NuxtLink :to="'/feedback/'+role.slug">Feedback required for {{ role.title }}s</NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    async asyncData({ $content, params, error }) {
+      const roles = await $content('roles')
+        .where({
+          slug: {
+            $ne: 'implementationlead'
+          }
+        })
+        .fetch()
+        .catch((err) => {
+          error({ statusCode: 404, message: 'Roles not recognised' })
+        })
+
+      return {
+        roles
+      }
+    },
+  }
+</script>
