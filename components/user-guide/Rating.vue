2a9b0bc122bce85235ea38aac9d68f8e7db32004
timberrr <timberrr@users.noreply.github.com>
2022-12-08 10:58:55 +0000

    added FA icons to replace emojis

--- components/user-guide/Rating.vue
+++ components/user-guide/Rating.vue
@@ -6,24 +6,31 @@
         How would you rate your experience of <b>{{ guideText }}</b
         >?
       </p>
-      <button
-        v-for="option in options"
-        :key="'option-' + option.value"
-        class="nhsuk-button nhsuk-u-padding-1 nhsuk-u-font-size-32"
-        :class="
-          score == option.value
-            ? 'nhsuk-button--primary'
-            : 'nhsuk-button--secondary'
-        "
-        :disabled="rated"
-        @click="rateGuide(option.value)"
-      >
-        {{ option.text }}
-      </button>
-      <div v-if="rated && score < 0 && !submitted" class="nhsuk-form-group">
+      <div>
+        <button
+          v-for="option in options"
+          :key="'option-' + option.value"
+          class="nhsuk-button step1-guide--rating_button nhsuk-u-padding-2 nhsuk-u-font-size-32"
+          :class="
+            score == option.value
+              ? 'nhsuk-button--primary'
+              : 'nhsuk-button--secondary'
+          "
+          :alt="option.text"
+          :title="option.text"
+          :disabled="rated"
+          @click="rateGuide(option.value)"
+        >
+          <FontAwesome :icon="option.icon" />
+        </button>
+      </div>
+      <div v-if="rated && score < 1 && !submitted" class="nhsuk-form-group">
         <label class="nhsuk-label" for="rating-comment">
-          Please tell us what issues you experienced:
+          Please share any information about your experience:
         </label>
+        <div class="nhsuk-hint">
+          e.g. any issues you encountered, things we could improve
+        </div>
         <textarea
           id="rating-comment"
           v-model="ratingComment"
@@ -52,6 +59,25 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import {
+  faFaceSmile,
+  faFaceFrown,
+  faFaceMeh,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+config.autoAddCss = false
+
+library.add(
+  faFaceSmile,
+  faFaceFrown,
+  faFaceMeh,
+)
+
+Vue.component('FontAwesome', FontAwesomeIcon)
+
 export default {
   props: {
     guide: String,
@@ -65,15 +91,18 @@ export default {
       submitted: false,
       options: [
         {
-          text: '😦',
+          icon: 'face-smile',
+          text: 'Bad',
           value: -1,
         },
         {
-          text: '😐',
+          icon: 'face-meh',
+          text: 'Average',
           value: 0,
         },
         {
-          text: '😀',
+          icon: 'face-frown',
+          text: 'Good',
           value: 1,
         },
       ],
@@ -93,7 +122,7 @@ export default {
       })
       this.rated = true
       this.score = score
-      this.submitted = score > -1
+      this.submitted = score > 0
     },
     submitComments() {
       if (this.ratingComment.length > 0) {
@@ -114,4 +143,20 @@ export default {
 @import 'node_modules/nhsuk-frontend/packages/components/textarea/textarea';
 @import 'node_modules/nhsuk-frontend/packages/components/label/label';
 @import 'node_modules/nhsuk-frontend/packages/components/card/card';
+
+.step1-guide--rating {
+  button.step1-guide--rating_button {
+    &:nth-child(even) {
+      border-radius: 0;
+    }
+    &:last-child {
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+    &:first-child {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+  }
+}
 </style>
