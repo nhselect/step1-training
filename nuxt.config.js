906fe62b65d5a1997a4b1a0f817e0cd2d7c9712b
timberrr <timberrr@users.noreply.github.com>
2023-10-05 13:58:39 +0100

    additional redirect for user guide

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,6 +89,7 @@ export default {
   netlify: {
     redirects: [
       { from: '/user-guide/:role', to: '/:role/user-guide' },
+      { from: '/user-guide/:role/:slug', to: '/:role/user-guide/:slug' },
       { from: '/feedback/:role', to: '/:role/feedback' },
       { from: '/process/:role', to: '/:role/process' },
       { from: '/roles-and-responsibilities/:role', to: '/:role/roles-and-responsibilities'},
